341175c02dbdefe8902c5713e96c27be47c6d0ab
Godric <godolphine21@gmail.com>
2021-11-01 20:12:45 +0000

    Added logic and view for users and user_details

--- webapp/urls.py
+++ webapp/urls.py
@@ -3,6 +3,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.index, name='index'),
-    path('users/', views.list_users, name='list_users')
+    path("", views.index, name="index"),
+    path("users/", views.list_users, name="list_users"),
+    path("users/<int:user_id>/", views.get_user, name="get_user")
 ]
--- webapp/views.py
+++ webapp/views.py
@@ -1,5 +1,6 @@
-from django.http import HttpResponse
+from django.http import HttpResponse, Http404
 from django.contrib.auth.models import User
+from django.shortcuts import render, get_object_or_404
 
 
 def index(request):
@@ -8,6 +9,13 @@ def index(request):
 
 def list_users(request):
     users = User.objects.all()
-    for user in users:
-        print(user.member)
-    return HttpResponse(users)
+    context = {
+        "users": users
+    }
+    return render(request, "webapp/users.html", context)
+
+
+def get_user(request, user_id):
+    user = get_object_or_404(User, pk=user_id)
+    print(user.userprofile.member.first_name)
+    return render(request, "webapp/user_details.html", {"user": user})
