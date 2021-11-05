6fa7f18a0b6f161d493335637988ab77237183a6
Godric <godolphine21@gmail.com>
2021-11-05 20:37:02 +0000

    Added logic and view for members and member_details

--- webapp/urls.py
+++ webapp/urls.py
@@ -5,5 +5,7 @@ from . import views
 urlpatterns = [
     path("", views.index, name="index"),
     path("users/", views.list_users, name="list_users"),
-    path("users/<int:user_id>/", views.get_user, name="get_user")
+    path("users/<int:user_id>/", views.get_user, name="get_user"),
+    path("members/", views.list_members, name="list_members"),
+    path("members/<int:member_id>/", views.get_member, name="get_member")
 ]
--- webapp/views.py
+++ webapp/views.py
@@ -1,6 +1,7 @@
 from django.http import HttpResponse, Http404
 from django.contrib.auth.models import User
 from django.shortcuts import render, get_object_or_404
+from .models import Member, Location, Contact
 
 
 def index(request):
@@ -17,5 +18,14 @@ def list_users(request):
 
 def get_user(request, user_id):
     user = get_object_or_404(User, pk=user_id)
-    print(user.userprofile.member.first_name)
     return render(request, "webapp/user_details.html", {"user": user})
+
+
+def list_members(request):
+    members = Member.objects.all()
+    return render(request, "webapp/members.html", {"members": members})
+
+
+def get_member(request, member_id):
+    member = get_object_or_404(Member, pk=member_id)
+    return render(request, "webapp/member_details.html", {"member": member})
